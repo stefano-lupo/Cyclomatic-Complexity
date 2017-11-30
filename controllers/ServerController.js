a25d0c6b724fc6c9ac57305c71a9e2c94ca18158
stefano <lupos@tcd.ie>
2017-11-30 14:23:34 +0000

    Fetches files for each commit asynchronously toeach other

--- controllers/ServerController.js
+++ controllers/ServerController.js
@@ -10,6 +10,8 @@ const GITHUB_BASE_URL = "https://api.github.com";
 // https://api.github.com/repos/stefano-lupo/DFS-File-System/git/trees/5f4b511df2949f58cbeda7687650b1c444db98b3
 
 
+//TODO: Make it able to handle mutliple caculateComplexity requests at a time
+
 /**
  * POST /api/complexity
  * body: {repoUrl, repoName, repoOwner}
@@ -19,46 +21,28 @@ export const calculateComplexity = async (req, res) => {
 
   // Get array of commits for this repo
   const { ok, status, response } = await makeRequest(`${GITHUB_BASE_URL}/repos/${repoOwner}/${repoName}/commits`);
-  const commits = response;
 
-  const repo = { commits, currentCommit: 0};
-  const allFiles = [];
-
-  // TODO: Make async with Promise.all()
-  await commits.forEach(async (commit) => {
-  // const commit = commits[0];
-
-    // Extract commit sha
-    const { sha } = commit;
-    // console.log(`${sha}: ${commit.commit.message}`);
-
-    // Get commit by its SHA
-    const endpoint = `${GITHUB_BASE_URL}/repos/${repoOwner}/${repoName}/git/commits/${sha}`;
-    // console.log(endpoint);
-    const response2 = await makeRequest(endpoint, "get");
-
-    // Extract tree SHA
-    const treeSha = response2.response.tree.sha;
-    // console.log(treeSha);
-
-    // Get file tree for this commit by its SHA
-    // Note recursive: pulls all of the files from subdirectories
-    // This leaves directories in the tree (type="tree") so filter these
-    const endpoint3 = `${GITHUB_BASE_URL}/repos/${repoOwner}/${repoName}/git/trees/${sha}?recursive=1`;
-    // console.log(endpoint3);
-    const resp = await makeRequest(endpoint3, "get");
-    let { tree } = resp.response;
-    const files = tree.filter(entry => entry.type === "blob");
-    // console.log(tree);
-    commit.files = files;
-    res.send(commit)
-    allFiles.push(files);
+  // Grab the sha and commit message and toss everything else
+  let commits = response.map(commit => {
+    return {sha: commit.sha, message: commit.commit.message};
   });
 
-  // console.log(allFiles);
-  // res.send(allFiles);
-  return;
 
+  // Get commits in chronological order (and just first 2 for debugging)
+  commits = commits.reverse().slice(0,2);
+
+
+  // Get array of promises of files for all commits
+  const work = commits.map(commit => {
+    return getFilesFromCommit(repoOwner, repoName, commit);
+  });
+
+  // Wait for all to resolve
+  await Promise.all(work);
+
+  console.log(commits);
+  res.send(commits);
+  return;
 
   repoUrl = repoUrl || `${GITHUB_BASE_URL}/${repoOwner}/${repoName}`;
   const body = {repoUrl, repoName, repoOwner};
@@ -80,6 +64,8 @@ export const calculateComplexity = async (req, res) => {
 
 const getFilesFromCommit = async (repoOwner, repoName, commit) => {
 
+  console.log(`Getting files for ${commit.message}`);
+
   // Extract commit sha
   const { sha } = commit;
   // console.log(`${sha}: ${commit.commit.message}`);
@@ -100,9 +86,9 @@ const getFilesFromCommit = async (repoOwner, repoName, commit) => {
   // console.log(endpoint3);
   const resp = await makeRequest(endpoint3, "get");
   let { tree } = resp.response;
-  const files = tree.filter(entry => entry.type === "blob");
-  // console.log(tree);
-  return files;
+  commit.files =  tree.filter(entry => entry.type === "blob");
+  console.log(`Set files for ${commit.message}`);
+
 };
 
 const getWorkerToCloneRepo = (worker, body) => {
